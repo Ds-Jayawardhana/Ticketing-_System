3e1f49e7bcecf918804a4629c11a433398676a7e
Disandu Sanhida Jayawardhane <disandu.20230469@iit.ac.lk>
2024-11-09 17:29:08 +0530

    Thread Creation Initilization Done

--- CLI/src/vendor.java
+++ CLI/src/vendor.java
@@ -1,3 +1,17 @@
-public class vendor {
+public class vendor implements Runnable{
 
+    private final TicketPool ticketPool;
+    private final int releaseRate;
+    private final int releaseInterval;
+
+    public vendor(TicketPool ticketPool, int releaseRate, int releaseInterval) {
+        this.ticketPool = ticketPool;
+        this.releaseRate = releaseRate;
+        this.releaseInterval = releaseInterval;
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
