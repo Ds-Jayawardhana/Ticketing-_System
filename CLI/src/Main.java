d039b0ea5f5fb2c556e9d3067dc229512812c9a3
Disandu Sanhida Jayawardhane <73415096+Ds-Jayawardhana@users.noreply.github.com>
2024-11-15 19:04:44 +0530

    main updated

--- CLI/src/Main.java
+++ CLI/src/Main.java
@@ -1,21 +1,57 @@
 import java.util.*;
 
 public class Main {
-    private final Scanner scanner = new Scanner(System.in);
-    private Configuration config;
+    private static final Scanner scanner = new Scanner(System.in);
+    private static Configuration config;
     private TicketPool ticketpool;
 
     public static void main(String[] args) {
         System.out.println("-------Welcome To Realtime Ticketing Booking System-------");
-
-
-
-
+        String prn_op = """
+                *************************
+                *      Menu Section     *
+                *************************
+                1. Start the System
+                2. Stop the System
+                3. Configure System
+                4. Display Configuration
+                5. Exit
+                """;
+                systemStart();
 
     }
+    public static void systemStart() {
+        while (true) {
+            System.out.println("Enter command: ");
+            String command = scanner.nextLine();
+            switch (command) {
+                case "1":
+                    System.out.println("Starting the System");
+                    break;
+                case "2":
+                    System.out.println("Stopping the System");
+                    break;
+                case "3":
+                    System.out.println("----------------------Delete Student------------------------");
+                    SystemConfig();
+                    System.out.println();
 
+                    break;
+                case "4":
+                    System.out.println("-----------------------Find Student--------------------------");
+                    config.displayConfig();
+                    System.out.println();
 
-    public void SystemConfig() {
+                    break;
+                case "5":
+                    System.out.println("------------------------Store students-------------------------");
+                    System.out.println();
+                    break;
+            }
+        }
+    }
+
+    public static void SystemConfig() {
         System.out.println("Please enter the following system Configuration values");
         System.out.println("Enter the Number of total tickets");
         config.setTotalTickets(scanner.nextInt());
@@ -29,8 +65,8 @@ public class Main {
         System.out.println("Enter the Number of total tickets");
         config.setNoCustomers(scanner.nextInt());
 
-
+            }
 
     }
-}
+
 
